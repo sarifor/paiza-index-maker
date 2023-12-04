e4ad4372a15cb56fae19dd0224444c47796b12d9
sarifor <iread1q84@gmail.com>
2023-12-04 18:19:01 +0900

    Troubleshooting: cannot get specific element

--- index.js
+++ index.js
@@ -5,20 +5,24 @@ const parsing = async (url) => {
   const list = [];
   const html = await axios.get(url);
 	const $ = cheerio.load(html.data);
-	const $coursList = $('#courses_section > div > div > div > main > div.courses_container > div > div'); // # is element id
+  const $coursList = $('#main > div.p-works-mondai > section > div.m-mondai-sets');
+	// const $coursList = $('#courses_section > div > div > div > main > div.courses_container > div > div'); // # is element id
 
 	$coursList.each((idx, el) => {
-		const title = $(el).find('.course_title').eq(0).text(); // 동명의 클래스 엘러먼트
-		const instructor = $(el).find('.instructor').text();
-		const img = $(el).find('.card-image > figure > img').attr('src');
-
+		const chapter = $(el).find('h2').text();
+    // const mondai = $(el).find('.m-mondai-sets__inner > li > div.m-mondai-set__inner').text(); // not work
 		list.push({
-			title,
-			instructor,
- 			img
-		});
-    console.log(list);		
+			chapter,
+      mondai,
+ 		});
 	});
+
+  return list;
+};
+
+async function main () {
+  const result = await parsing('https://paiza.jp/works/mondai');
+  console.log(result);
 }
 
-parsing('https://www.inflearn.com/courses?s=%ED%83%80%EC%9E%85%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8');
+main();
